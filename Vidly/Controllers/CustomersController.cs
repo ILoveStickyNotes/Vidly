9d2c48f6bd6c22915787f3d9a668a9a6f1af02d8
Michael Bui <bui.michael652@gmail.com>
2017-01-27 15:15:06 -0500

    Setup

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
